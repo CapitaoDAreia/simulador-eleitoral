57b92a5fc8e51dde117ce26df615c51445c2c7f2
Igor Silva <danieligord7@hotmail.com>
2022-07-30 12:31:33 -0300

    Troca de displays finalizada

--- script/_main.js
+++ script/_main.js
@@ -1,28 +1,38 @@
+/*
+    MAIN - Inicia o simulador, efetua a troca de display.
+*/
 class Simulador{
     constructor(){
-        this.body = document.querySelector('.all-content');
-        this.popup = document.querySelector('.popup');
-        this.buttonCadastro = document.querySelectorAll('.main-btn-cadastrar');
+        this.main = document.querySelectorAll('.main > article');
     }
-
     abreCadastro(){
-        this.adicionaEvento(this.buttonCadastro, 'click');
+        this.main[2].classList.add('ocultar-article');
+        this.main[1].classList.add('ocultar-article');
+        this.main[0].classList.remove('ocultar-article'); //Tela de cadastro
     }
-
+    abreVotar(){
+        this.main[2].classList.add('ocultar-article');
+        this.main[0].classList.add('ocultar-article');
+        this.main[1].classList.remove('ocultar-article'); //Tela de votação
+    }
+    abreApurar(){
+        this.main[1].classList.add('ocultar-article');
+        this.main[0].classList.add('ocultar-article');
+        this.main[2].classList.remove('ocultar-article'); //Tela de apuração
+    }
     adicionaEvento(){
         document.addEventListener('click', e=>{
             const event = e.target;
-            console.log(event)
-            
+            if(event.classList.contains('main-btn-cadastrar')) this.abreCadastro();
+            if(event.classList.contains('main-btn-votar')) this.abreVotar();
+            if(event.classList.contains('main-btn-apurar')) this.abreApurar(); 
         })
     }
-
     iniciaSimulador(){
         this.adicionaEvento();
     }
 }
-
 const simulador = new Simulador();
 simulador.iniciaSimulador();
-console.log(document)
+
 
